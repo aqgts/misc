6965118e4c3ca96e5f7939299bc17b71b6c55ddb
aq <aqgtssize@gmail.com>
2017-11-12 23:28:09 +0900

    Squashed '46thmmd/lib/aqgts-lib/' content from commit ddd7807
    git-subtree-dir: 46thmmd/lib/aqgts-lib
    git-subtree-split: ddd780789fc67f9a87c2764bf466ab7f394faeba

---
+++ src/directed-line-segment-3d-utils.js
@@ -0,0 +1,7 @@
+import DirectedLineSegment2DUtils from "./directed-line-segment-2d-utils";
+
+export default {
+  connect(originalLineSegments) {
+    return DirectedLineSegment2DUtils.connect(originalLineSegments);
+  }
+};
